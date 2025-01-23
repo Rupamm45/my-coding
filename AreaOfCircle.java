8d684799fd0fa97fcf288cd05bafb95a704a5359
Rupam Mukherjee <rupamm963@gmail.com>
2025-01-23 17:31:23 +0530

    First Ever Commit

---
+++ AreaOfCircle.java
@@ -0,0 +1,10 @@
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        double r=5.9;
+//        double a=3.1415*r*r;
+//        System.out.println("Area of Circle is:"+a);
+//        System.out.println(a);;
+        double vol=(4*3.14*r*r*r)/3;
+        System.out.println(vol);
+    }
+}
